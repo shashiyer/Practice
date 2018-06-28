dc474d0b6530b2a08a08fcbb7e2e42a0beb9276e
shashiyer <40667942+shashiyer@users.noreply.github.com>
2018-06-28 23:06:50 +0530

    Create Displaypattern.java
    Displays * patterns

---
+++ Displaypattern.java
@@ -0,0 +1,14 @@
+class Displaypattern
+{
+  public static void main(String[] args)
+  {
+    for(int i = 0; i < 5; i++)
+    {
+      for(int j = 0; j < 10; j++)
+      {
+        System.out.print("*");
+      }
+    System.out.println();
+    }
+}
+    
